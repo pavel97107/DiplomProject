d7a619c907693d1ddb633267f195c45d56cbdfb3
Pavel Siarebrany <pavel97107@gmail.com>
2020-03-13 11:04:44 +0300

    mission_sendForm_moreWindow

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -1,74 +1,73 @@
-const sendForm = (selector) => {
-    const form = document.querySelector(selector);
-    const loadMessage = 'Загрузка....';
-    const error = 'Что то пошло не так!';
-    const statusMessage = document.createElement('div');
-    statusMessage.style.fontSize = '20px';
-    statusMessage.style.fontWeight = '500';
-    const elementForm = document.querySelectorAll('input');
+const sendForm = selector => {
+  const form = document.querySelector(selector);
+  const loadMessage = "Загрузка....";
+  const error = "Что то пошло не так!";
+  const statusMessage = document.createElement("div");
+  statusMessage.style.fontSize = "20px";
+  statusMessage.style.fontWeight = "500";
+  const elementForm = document.querySelectorAll("input");
 
+  form.addEventListener("input", event => {
+    let target = event.target;
+    let text = target.value;
 
-    form.addEventListener('input', (event) => {
-        let target = event.target;
-        let text = target.value;
+    if (target.name === "user_name") {
+      target.value = text.replace(/[^а-яА-я]/g, "");
+    }
+    if (target.name === "user_phone") {
+      target.value = text.replace(/[^0-9\+\(\)\-]/g, "");
+    }
+  });
 
-        if (target.name === 'user_name') {
-            target.value = text.replace(/[^а-яА-я]/g, '');
-        }
-        if (target.name === 'user_phone') {
-            target.value = text.replace(/[^0-9\+\(\)\-]/g, '');
-        }
+  const resetInput = () => {
+    elementForm.forEach(item => {
+      item.value = "";
+    });
+  };
+
+  const successMessage = response => {
+    if (response.status !== 200) {
+      statusMessage.textContent = error;
+      setTimeout(() => {
+        statusMessage.remove();
+        resetInput();
+      }, 6000);
+      throw new Error("status network not 200");
+    } else {
+      statusMessage.textContent = "Спасибо, мы скоро свяжемся с вами!";
+      resetInput();
+      setTimeout(() => {
+        statusMessage.remove();
+      }, 3000);
+    }
+  };
+
+  form.addEventListener("submit", event => {
+    event.preventDefault();
+    form.append(statusMessage);
+    statusMessage.textContent = loadMessage;
+
+    const formData = new FormData(form);
+
+    let body = {};
+
+    formData.forEach((item, value) => {
+      body[value] = item;
     });
 
-    const resetInput = () => {
-        elementForm.forEach((item) => {
-            item.value = '';
-        });
-    };
+    postData(body)
+      .then(successMessage)
+      .catch();
+  });
 
-    const successMessage = (response) => {
-        if (response.status !== 200) {
-            statusMessage.textContent = error;
-            throw new Error('status network not 200');
-        } else {
-            statusMessage.textContent = 'Спасибо, мы скоро свяжемся с вами!';
-            resetInput();
-            setTimeout(() => {
-                statusMessage.remove();
-            }, 3000);
-        }
-    };
-
-
-    form.addEventListener('submit', (event) => {
-        event.preventDefault();
-        form.append(statusMessage);
-        statusMessage.textContent = loadMessage;
-
-        const formData = new FormData(form);
-
-        let body = {};
-
-        formData.forEach((item, value) => {
-            body[value] = item;
-        });
-
-        postData(body).then(successMessage).catch();
+  const postData = body => {
+    return fetch("./server.php", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(body)
     });
-
-
-    const postData = (body) => {
-        return fetch("./server.php", {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(body)
-        });
-    };
-
-
-
-
+  };
 };
 export default sendForm;
